3ea674bd66d7a099964f6e6ceb9033ed41e2f8fe
Clément Garbay <clementgarbay@gmail.com>
2018-03-04 11:27:07 +0100

    Switch to Spek and Kluent for unit tests

---
+++ src/test/kotlin/organization/command/AddOrganizationSpec.kt
@@ -0,0 +1,58 @@
+package organization.command
+
+import core.infrastructure.bus.command.CommandBus
+import core.infrastructure.persistence.InMemoryStore
+import org.amshove.kluent.shouldContain
+import org.amshove.kluent.shouldEqual
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import organization.command.handler.AddOrganizationHandler
+import organization.domain.Organization
+import java.util.*
+
+/**
+ * @author Clément Garbay
+ */
+@RunWith(JUnitPlatform::class)
+object AddOrganizationSpec: Spek({
+
+    given("a store and a command bus") {
+        val store = InMemoryStore<UUID, Organization>()
+        val handlers = listOf(AddOrganizationHandler(store))
+        val commandBus = CommandBus(handlers)
+
+        val name = "Name"
+        val name2 = "Name 2"
+        val secret = true
+        val secret2 = false
+
+        on("dispatch two AddOrganization commands") {
+            commandBus.dispatch(AddOrganization(name, secret))
+            commandBus.dispatch(AddOrganization(name2, secret2))
+
+            it("should have the new organizations") {
+                store.getAll().map { it.name } shouldContain name
+                store.getAll().map { it.name } shouldContain name2
+                store.getAll().size shouldEqual 2
+            }
+        }
+    }
+
+    given("a store and a command bus without handler") {
+        val store = InMemoryStore<UUID, Organization>()
+        val commandBus = CommandBus(emptyList())
+
+        on("dispatch an AddOrganization command") {
+            commandBus.dispatch(AddOrganization("Name", true))
+
+            it("should not have the new organization") {
+                store.getAll().size shouldEqual 0
+            }
+        }
+    }
+
+})
---
+++ src/test/kotlin/organization/command/RenameOrganizationSpec.kt
@@ -0,0 +1,85 @@
+package organization.command
+
+import core.infrastructure.bus.command.CommandBus
+import core.infrastructure.persistence.InMemoryStore
+import org.amshove.kluent.shouldEqual
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import organization.command.handler.RenameOrganizationHandler
+import organization.domain.Organization
+import java.util.*
+
+/**
+ * @author Clément Garbay
+ */
+@RunWith(JUnitPlatform::class)
+object RenameOrganizationSpec: Spek({
+
+    given("a store containing an organization and a command bus") {
+        val store = InMemoryStore<UUID, Organization>()
+        val id = UUID.randomUUID()
+        store.add(Organization(id, "Name", false))
+
+        val handlers = listOf(RenameOrganizationHandler(store))
+        val commandBus = CommandBus(handlers)
+
+        val newName = "New Name"
+
+        on("dispatch a RenameOrganization command") {
+            commandBus.dispatch(RenameOrganization(id, newName))
+
+            it("should have the organization with the new name") {
+                store.get(id)!!.name shouldEqual newName
+                store.getAll().size shouldEqual 1
+            }
+        }
+    }
+
+    given("a store containing three organizations and a command bus") {
+        val store = InMemoryStore<UUID, Organization>()
+        val id = UUID.randomUUID()
+        val id2 = UUID.randomUUID()
+        val name1 = "Name 1"
+        store.add(Organization(id, "Name", false))
+        store.add(Organization(id2, name1, true))
+        store.add(Organization(UUID.randomUUID(), "Name 2", false))
+
+        val handlers = listOf(RenameOrganizationHandler(store))
+        val commandBus = CommandBus(handlers)
+
+        val newName = "New Name"
+
+        on("dispatch a RenameOrganization command") {
+            commandBus.dispatch(RenameOrganization(id, newName))
+
+            it("should not rename others organizations") {
+                store.get(id)!!.name shouldEqual newName
+                store.get(id2)!!.name shouldEqual name1
+                store.getAll().size shouldEqual 3
+            }
+        }
+    }
+
+    given("a store and a command bus without handler") {
+        val store = InMemoryStore<UUID, Organization>()
+        val id = UUID.randomUUID()
+        val name = "Name"
+        store.add(Organization(id, name, false))
+
+        val commandBus = CommandBus(emptyList())
+
+        on("dispatch a RenameOrganization command") {
+            commandBus.dispatch(RenameOrganization(id, "New Name"))
+
+            it("should not rename others organizations") {
+                store.get(id)!!.name shouldEqual name
+                store.getAll().size shouldEqual 1
+            }
+        }
+    }
+
+})
